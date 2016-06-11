206488682b7e88c20da7864b8910a9329789e175
Ri Caragol <ri.caragol@localstake.com>
2016-06-11 18:19:52 -0400

    Section 1: Into to React Completed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDO1GyF4lnIgdh4cF5XuCSAAB307HRUkmo';
+// Create a new component
+//compoenne should produce HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return (
+    <div>
+    <SearchBar />
+  </div>
+  );
+}
+//Take this compoenent's html and put it on the page.
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
