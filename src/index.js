dd4995528ffefa8e2b2a5c73f62cf33c2ee84e89
Ri Caragol <ri.caragol@localstake.com>
2016-06-12 21:28:22 -0400

    youtube dynamic search and listing working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDO1GyF4lnIgdh4cF5XuCSAAB307HRUkmo';
+
 // Create a new component
 //compoenne should produce HTML
 
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-  </div>
-  );
+class App extends Component {
+  constructor(props)  {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 //Take this compoenent's html and put it on the page.
 
